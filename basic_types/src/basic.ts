af92e1a9ce1646f83d887b2977c4ed3d483a0696
asifalam515 <asibulalam515@gmail.com>
2024-12-24 15:58:55 +0600

    tuple added

--- basic_types/src/basic.ts
+++ basic_types/src/basic.ts
@@ -1,5 +1,25 @@
 {
+//  variableName:dataType = value
+
     let name:string = "asibul alam"
     const roll: number = 12
     const isRaining:boolean = false
+    let x:undefined = undefined
+    let y :null = null
+    // any type(should not use )
+    let d
+    d  = 124
+    d = "str"
+    d = true
+
+
+    //Array 
+    const friends :string[] = ["najmul",'asif','sakib']
+    const passedRoolList :number[] = [123,2,33,56]
+    // tuple =>special type of array -> maintain order of values
+    // x axis and y axis 
+    let coordinates :[number,number]= [1,5]
+    let ageNmae:[number,string] = [25,"asif"]
+
+
 }
