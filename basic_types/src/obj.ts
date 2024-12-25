af23e1b69121de6e83040d7673ed2ac2c4a43379
asifalam515 <asibulalam515@gmail.com>
2024-12-25 08:52:12 +0600

    type literal added

---
+++ basic_types/src/obj.ts
@@ -0,0 +1,14 @@
+{
+    const user:{
+        firstName:string;
+        middleName?:string; //optional type
+        lastName:string;
+        isMarried?:boolean;
+        company:"Programming Hero"
+    } = {
+        firstName:"asibul",
+        isMarried:true,
+        lastName:"alam",
+        company:"Programming Hero"
+    }
+}
