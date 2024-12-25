dc0a05f854cc4c5bcb64403bb8166921e6fe9390
asifalam515 <asibulalam515@gmail.com>
2024-12-25 11:41:40 +0600

    ternary added

---
+++ basic_types/src/ternary.ts
@@ -0,0 +1,18 @@
+{
+    
+    // ?
+
+    // ternary
+const age :number = 73
+if(age>=18){
+console.log("You are adult")
+} else{
+    console.log("You are child")
+}
+
+// ternary operator:
+const isAdult = age>=18?"adult age":"Young age"
+// console.log(isAdult)
+
+
+}
