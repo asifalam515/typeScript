16825558f7aae54f6f032a67cba1872d4abdadd7
asifalam515 <asibulalam515@gmail.com>
2024-12-26 19:59:37 +0600

    assertion

--- advance_types/assertion.ts
+++ advance_types/assertion.ts
@@ -11,16 +11,17 @@ x  =12;
 (x as number).toFixed(2)
 
 // 
-const kgTogm = (value :string | number)=>{
+const kgTogm = (value :string | number):string|number|undefined=>{
     if(typeof value ==="string"){
         const convertedValue = parseFloat(value)
-        return convertedValue
+        return (`convertedValue is ${convertedValue}`)
     }
     if(typeof value ==="number"){
         return value
     }
 
 }
-
+const result1 =  kgTogm(1000) as number
+const result = kgTogm("10000") as string
 
 }
