41ca81ec1cf12f3e5dc3da5ee524d93089b2a016
asifalam515 <asibulalam515@gmail.com>
2024-12-28 21:53:52 +0600

    generic added

---
+++ advance_types/generic.ts
@@ -0,0 +1,33 @@
+{
+type GenericArray<T> = Array<T>
+    const rollNumbers :GenericArray<number>= [213,67,2,2,52]
+    const mentors :GenericArray<string>= ["x","y","sakib","rakib"]
+    const bollArray :GenericArray<boolean> = [true,false ,true]
+
+
+    // generice array in array of object
+    const user:GenericArray<{name:string,age?:number,role?:string}> = [
+        {name:"asif",
+            age:23,
+        },
+        {name:"mahub",
+            age:25
+        },
+        {
+            name:"najmul",
+            role:"admin"
+        }
+    ]
+
+
+    // tuple withe generics
+    type GenericTuple <X,Y>=[X,Y]
+     const men:GenericTuple<string,string>=["Mr x","Mrs Y"]  
+   
+    const userWitId :GenericTuple<number,{name:string,email:string}>=[
+        1231,{
+            name:"asif",
+            email:"asif@gmail.com"
+        }
+    ]
+}
